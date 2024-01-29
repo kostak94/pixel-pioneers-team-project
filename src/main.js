506ea85a60461a70161ba7945929a12c10c53d86
Yaroslav Kostak <slavakostak2010@gmail.com>
2024-01-29 17:27:07 +0100

    fixed modal animation

--- src/main.js
+++ src/main.js
@@ -2,20 +2,24 @@ let headerBtns = document.querySelector('.open-menu-mobile-btn');
 let backdrop = document.querySelector('.backdrop');
 let closebtn = document.querySelector('.modal-button-close');
 let newPage = document.querySelectorAll('.list-item-link');
+let closeModal = document.querySelectorAll('.modal-order-link');
 
-for (let link of newPage){
-    link.onclick = function() {
-        backdrop.classList.remove('active');
-    }
-}
-
-headerBtns.onclick = function() {
-    backdrop.classList.add('active');
-}
-
-closebtn.onclick = function() {
+for (let link of newPage) {
+  link.onclick = function () {
     backdrop.classList.remove('active');
+  };
 }
 
+for (let close of closeModal) {
+  close.onclick = function () {
+    backdrop.classList.remove('active');
+  };
+}
 
+headerBtns.onclick = function () {
+  backdrop.classList.add('active');
+};
 
+closebtn.onclick = function () {
+  backdrop.classList.remove('active');
+};
